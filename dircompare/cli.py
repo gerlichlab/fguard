c5d48e6bd195375793d2a87eea7a1cbc193a5f6c
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2022-03-01 16:55:29 +0100

    renamed stat to scan

--- dircompare/cli.py
+++ dircompare/cli.py
@@ -1,8 +1,7 @@
 """Command line interface for dircompare"""
 import os
-from unicodedata import name
 import click
-from datetime import date, datetime
+from datetime import datetime
 from dircompare.colllectors import CollectionResult, FlatCollector, DATEFORMAT
 from dircompare.triggers import FilesMissingTrigger
 from dircompare.actions import ACTIONMAP
@@ -17,7 +16,7 @@ def cli():
 @click.command()
 @click.option("--outputDir", default="./", help="Output directory for scan file")
 @click.argument("root_directories", nargs=-1)
-def stat(root_directories, outputdir):
+def scan(root_directories, outputdir):
     collector = FlatCollector(root_directories)
     collector.collect_files()
     collector.save(outputdir)
@@ -26,7 +25,7 @@ def stat(root_directories, outputdir):
 @click.option("--newScan", default=None, help="Defaults to the newest scan in the current working directory")
 @click.option("--oldScan", default=None, help="Defaults to the second newest scan in the current working directory")
 @click.option("--actions", "action_names", default=["stdout"], help="Actions to be performed when triggers fire", multiple=True)
-@click.option("--threshhold", default=50, help="Threshold above which actions are triggered")
+@click.option("--threshhold", default=50, help="Threshold of missing files above which actions are triggered")
 def missing_files(newscan, oldscan, action_names, threshhold):
     # get actions
     actions= []
@@ -51,5 +50,5 @@ def missing_files(newscan, oldscan, action_names, threshhold):
     trigger = FilesMissingTrigger(actions=actions, number_threshold=threshhold)
     trigger.inspect(old_result, new_result)
 
-cli.add_command(stat)
+cli.add_command(scan)
 cli.add_command(missing_files)

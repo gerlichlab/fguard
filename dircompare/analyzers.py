0082468553f3380594e94d2e42e8fd0dd26723db
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2022-03-01 13:42:14 +0100

    added tests for analyzer

---
+++ dircompare/analyzers.py
@@ -0,0 +1,58 @@
+"""Classes for analyzing a directory tree in terms of contained files"""
+import os
+import pickle
+import logging
+from typing import List
+
+
+class FlatAnalyzer:
+    """Walks a list of root directories
+    and stores the absolute path to containing files
+    in a dictionary with the associated values being information
+    about the files."""
+
+    def __init__(self, rootDirectories: List[str], log_level: str = "INFO") -> None:
+        self._checkDirectories(rootDirectories)
+        self.rootDirectories = rootDirectories
+        self.files = {}
+        self.logger = logging.getLogger()
+        self.logger.setLevel(log_level)
+
+    def _checkDirectories(self, directories: List[str]) -> None:
+        """Checks whether the supplied directories exist and whether
+        it is supplied as an absolute path"""
+        for directory in directories:
+            if not os.path.isdir(directory):
+                raise ValueError(f"Path is not a directory: {directory}")
+            if not os.path.isabs(directory):
+                raise ValueError(f"Path {directory} should be supplied as absolute")
+
+    def collect_files(self):
+        """Walks specified root directories and
+        puts found files with associated statistics
+        into a flat dictionary"""
+        for directory in self.rootDirectories:
+            self.logger.info(f"Collecting files from {directory}")
+            for dirpath, dirnames, files in os.walk(directory):
+                for file in files:
+                    filename = os.path.join(dirpath, file)
+                    file_stats = os.stat(filename)
+                    file_info = {
+                        "size": file_stats.st_size,
+                        "modified_date": file_stats.st_mtime,
+                        "created_date": file_stats.st_ctime,
+                        "user_id": file_stats.st_uid,
+                    }
+                    self.logger.debug(f"Processing {filename}")
+                    self.files[filename] = file_info
+        self.logger.info(f"Found {len(self.files.keys())} files")
+
+    def get_file_stats(self):
+        """Returns dictionary of file statistics"""
+        return self.files
+
+    def save_file_state(self, output_file):
+        """writes collected files to the output file"""
+        if os.path.isfile(output_file):
+            raise ValueError("Output file already exists!")
+        pickle.dump(self.files, open("output_file", "wb"))
---
+++ dircompare/tests/test_analyzer.py
@@ -0,0 +1,55 @@
+"""tests for the analyzer classes"""
+import unittest
+import os
+from dircompare.analyzers import FlatAnalyzer
+
+class TestFlatAnalyzer(unittest.TestCase):
+    """Testsuite for flat analyzer"""
+
+    def test_non_abs_directories_rejected(self):
+        """Tests whether non absolute directories are rejected"""
+        relativeDirectory = "dircompare/tests/testfiles"
+        badcall = lambda: FlatAnalyzer([relativeDirectory])
+        self.assertRaises(ValueError, badcall)
+
+
+    def test_non_existing_directory_rejected(self):
+        file_path = os.path.abspath(__file__)
+        test_dir = os.path.join(file_path, "testfilesxx")
+        badcall = lambda: FlatAnalyzer([test_dir])
+        self.assertRaises(ValueError, badcall)
+
+    def test_existing_directory_accepted(self):
+        file_path = os.path.abspath(__file__)
+        directory = os.path.dirname(file_path)
+        test_dir = os.path.join(directory, "testfiles")
+        # instantiate
+        FlatAnalyzer([test_dir])
+
+    def test_correct_files_found(self):
+        """"Tests whether correct files are found"""
+        file_path = os.path.abspath(__file__)
+        directory = os.path.dirname(file_path)
+        test_dir1 = os.path.join(directory, "testfiles")
+        test_dir2 = os.path.join(directory, "testfiles2")
+        # instantiate
+        analyzer = FlatAnalyzer([test_dir1, test_dir2])
+        # collect
+        analyzer.collect_files()
+        result = analyzer.get_file_stats()
+        # compare to expected
+        expected_keys = set(
+            [
+                os.path.join(test_dir1, "test1.txt"),
+                os.path.join(test_dir1, "test2.txt"),
+                os.path.join(test_dir1, "test3.txt"),
+                os.path.join(test_dir2, "test4.txt"),
+                os.path.join(test_dir2, "test5.txt"),
+                os.path.join(test_dir2, "testfiles3/test6.txt"),
+            ]
+        )
+        self.assertEqual(set(result.keys()), expected_keys)
+
+
+if __name__ == "__main__":
+    res = unittest.main(verbosity=3, exit=False)

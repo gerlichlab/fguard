6bae34fffd3df85bc50d61f770a394c070b74c38
Michael Mitter <michael.mitter@imba.oeaw.ac.at>
2022-03-03 14:25:35 +0100

    added experiment missing trigger

--- fguard/tests/test_triggers.py
+++ fguard/tests/test_triggers.py
@@ -1,10 +1,9 @@
 """Test triggers"""
 
 import unittest
-import os
 from datetime import datetime
 from unittest.mock import MagicMock
-from fguard.triggers import FilesMissingTrigger
+from fguard.triggers import FilesMissingTrigger, ExperimentsMissingTrigger
 from fguard.colllectors import CollectionResult
 
 
@@ -53,7 +52,9 @@ class TestFilesMissingTrigger(unittest.TestCase):
         # check triggering
         trigger = FilesMissingTrigger(number_threshold=1, actions=[mock_action])
         message = trigger.inspect(old_result, new_result)
-        self.assertEqual(message["experiments_affected"], {"004211": 1, "004212": 1})
+        self.assertEqual(
+            message["details"]["experiments_affected"], {"004211": 1, "004212": 1}
+        )
 
     def test_malformed_experiments_not_found(self):
         """Tests that malformed experimetns are excluded"""
@@ -76,8 +77,65 @@ class TestFilesMissingTrigger(unittest.TestCase):
         # check triggering
         trigger = FilesMissingTrigger(number_threshold=1, actions=[mock_action])
         message = trigger.inspect(old_result, new_result)
-        self.assertEqual(message["experiments_affected"], {})
+        self.assertEqual(message["details"]["experiments_affected"], {})
 
+
+class TestExperimentsMissingTrigger(unittest.TestCase):
+    """Test for experiments missing trigger"""
+
+    def test_triggered_when_experiments_missing(self):
+        """Test whether is correctly triggered when experiments are missing"""
+        new_files = {"test1": "asdf", "test2": "fdsa"}
+        old_files = {
+            "test1": "asdf",
+            "test2": "fdsa",
+            "/groups/gerlich/experiments/Experiments_004200/004211/test3": "asdf",
+            "/groups/gerlich/experiments/Experiments_004200/004212/test4": "fdsa",
+        }
+        # create results
+        new_result = CollectionResult(
+            new_files, directories_scanned=["test"], date=datetime.now()
+        )
+        old_result = CollectionResult(
+            old_files, directories_scanned=["test"], date=datetime.now()
+        )
+        # set up mocking
+        mock_action = MagicMock()
+        perform_mock = MagicMock()
+        mock_action.perform = perform_mock
+        # check triggering
+        trigger = ExperimentsMissingTrigger(actions=[mock_action])
+        message = trigger.inspect(old_result, new_result)
+        self.assertEqual(
+            sorted(message["details"]["experiments_missing"]), sorted(["004211", "004212"])
+        )
+        perform_mock.assert_called()
+
+    def test_not_triggered_when_files_missing(self):
+        """Test whether is correctly triggered when experiments are missing"""
+        new_files = {
+            "/groups/gerlich/experiments/Experiments_004200/004211/test3": "asdf"
+        }
+        old_files = {
+            "/groups/gerlich/experiments/Experiments_004200/004211/test3": "asdf",
+            "/groups/gerlich/experiments/Experiments_004200/004211/test4": "fdsa",
+        }
+        # create results
+        new_result = CollectionResult(
+            new_files, directories_scanned=["test"], date=datetime.now()
+        )
+        old_result = CollectionResult(
+            old_files, directories_scanned=["test"], date=datetime.now()
+        )
+        # set up mocking
+        mock_action = MagicMock()
+        perform_mock = MagicMock()
+        mock_action.perform = perform_mock
+        # check triggering
+        trigger = ExperimentsMissingTrigger(actions=[mock_action])
+        message = trigger.inspect(old_result, new_result)
+        self.assertEqual(message, {})
+        perform_mock.assert_not_called()
 
 if __name__ == "__main__":
     res = unittest.main(verbosity=3, exit=False)
--- fguard/triggers.py
+++ fguard/triggers.py
@@ -1,13 +1,11 @@
 """Classes for triggering actions"""
-from email import message
 import re
 from abc import ABC, abstractmethod
-import json
-from typing import List
+from typing import List, Set
 from fguard.actions import BaseAction
 from fguard.colllectors import CollectionResult
 
-EXPERIMENT_NUMBER = r"Experiments_(\d{6})/(\d{6})"
+EXPERIMENT_NUMBER = r"Experiments_(\d{6})/(\d{6})/"
 
 
 class BaseTrigger(ABC):
@@ -15,12 +13,12 @@ class BaseTrigger(ABC):
     its interface"""
 
     @abstractmethod
-    def inspect(self, old_state: dict, new_state: dict):
+    def inspect(self, old_state: CollectionResult, new_state: CollectionResult):
         pass
 
 
 class FilesMissingTrigger(BaseTrigger):
-    """Will trigger an action when
+    """Will trigger an actions when
     more than a specified number of files are missing"""
 
     def __init__(self, actions: List[BaseAction], number_threshold: int = 100):
@@ -61,17 +59,67 @@ class FilesMissingTrigger(BaseTrigger):
                         experiments_affected[experiment_number] = 1
         # check whether actions should be performed
         if missing_files_number > self.number_threshold:
-
+            message = self._construct_message(
+                missing_files_number,
+                old_state,
+                new_state,
+                experiments_affected,
+            )
             for action in self.actions:
-                action.perform(
-                    self._construct_message(
-                        missing_files_number,
-                        old_state,
-                        new_state,
-                        experiments_affected,
-                    )
-                )
+                action.perform(message)
             return message
+        return {}
+
+class ExperimentsMissingTrigger(BaseTrigger):
+    """Will trigger actions when an experiment is missing"""
+    def __init__(self, actions: List[BaseAction]):
+        self.actions = actions
+
+    def _get_experiments(self, state: CollectionResult) -> Set[int]:
+        experiments = set()
+        for file in state.get_files():
+            matched_experiment = re.findall(EXPERIMENT_NUMBER, file)
+            if len(matched_experiment) > 0 and len(matched_experiment[0]) > 1:
+                experiment_number = matched_experiment[0][1]
+                experiments.add(experiment_number)
+        return experiments
+
+    def _construct_message(
+        self, missing_experiments, old_state, new_state
+    ):
+        return {
+            "title": "Missing experiments detected!",
+            "description": f"There where {len(missing_experiments)} missing experiments detected between a scan performed at {new_state.get_date()} and {old_state.get_date()}.",
+            "subject": "Missing files detected",
+            "details": {
+                "missing_experiment_number": len(missing_experiments),
+                "experiments_missing": missing_experiments,
+                "new_directories_scanned": new_state.get_directories_scanned(),
+                "old_directories_scanned": old_state.get_directories_scanned(),
+                "new_date[utc]": str(new_state.get_date()),
+                "old_date[utc": str(old_state.get_date()),
+            },
+        }
+
+    def inspect(self, old_state: CollectionResult, new_state: CollectionResult):
+        old_experiments = self._get_experiments(old_state)
+        new_experiments = self._get_experiments(new_state)
+        # go through experiments and check missing ones
+        missing_experiments = []
+        for experiment in old_experiments:
+            if experiment not in new_experiments:
+                missing_experiments.append(experiment)
+        if len(missing_experiments) > 0:
+            message = self._construct_message(
+                missing_experiments,
+                old_state,
+                new_state,
+            )
+            for action in self.actions:
+                action.perform(message)
+            return message
+        return {}
+
 
 
 TRIGGERMAP = {"files_missing": FilesMissingTrigger}
